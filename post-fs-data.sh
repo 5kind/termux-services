d968e244b0c43f1c4e6807014065fe1d9fa8285a
5kind <shikabane320@gmail.com>
2025-07-31 21:42:08 +0800

    fix: Update post-fs-data.sh to use MODPATH for configuration file sourcing

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,6 +1,8 @@
 # post-fs-data.sh
-[ -f /etc/rc.conf ] && . /etc/rc.conf
+[ -f "$MODPATH/system/etc/profile.d/runit.sh" ] && \
+    . "$MODPATH/system/etc/profile.d/runit.sh"
 # Log Rotation & Redirection
 LOGFILE="$LOGDIR/$MODID.log"
 [ -f "$LOGFILE" ] && mv -f "$LOGFILE" "$LOGFILE.bak"
-[ -f /etc/runit/1 ] && . /etc/runit/1 > "$LOGFILE" 2>&1
+[ -f "$MODPATH/system/etc/runit/1" ] && \
+    . "$MODPATH/system/etc/runit/1" > "$LOGFILE" 2>&1
