36effb93269a626849d3a9ec115c4669b35ba805
5kind <shikabane320@gmail.com>
2025-07-31 19:34:17 +0800

    fix: Fix install script to set permissions for scripts

--- common/install.sh
+++ common/install.sh
@@ -1,3 +1,7 @@
+for SCRIPT in "$MODDIR/system/bin"/* "$MODDIR/system/etc/rc.local"; do
+    set_perm "$SCRIPT" 0 0 0755
+done
+
 cp -r "$MODDIR/common/termux-services" /data/adb
 chmod +x /data/adb/termux-services/svlogger
 
@@ -6,3 +10,7 @@ PREFIX=/data/data/com.termux/files/usr
 
 [ -n "$PREFIX" ] && [ -f "$PREFIX/etc/fstab" ] && \
 ln -sf "$PREFIX/etc/fstab" "$MODDIR/system/etc/fstab"
+
+ui_print "Modifying rc.conf in /data/adb/$MODID to customize Termux services..."
+ui_print "# /data/adb/$MODID/rc.conf"
+cat /data/adb/$MODID/rc.conf
